c81687b5aed3e7fa06ac1730e25a8857c307d132
JunLyul Yang <sjn3649@gmail.com>
2023-04-29 09:20:22 +0900

    HTML 구조적 크롤링 방식으로 수정 - 다만, 이게 계속 유효할거란 보장이 없기에 차라리 수동으로 class명 따서 크롤링 하는게 편할 수 있음...

--- crawl_module/base/crawller.py
+++ crawl_module/base/crawller.py
@@ -31,14 +31,18 @@ def crawl_with_scroll_search_page(driver, target_url: str):
     # 페이지 소스코드 추출
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    # 채용 공고 추출
+    # 채용 별 div 요소 클래스 명 추출
     jobs = []
-    job_cards = soup.find_all('div', {'class': 'fKzIXW'})
+    job_section = soup.select('main section')[2]
+    card_class_name = job_section.find(recursive=False).get('class')[-1]
+    job_cards = soup.find_all('div', {'class': card_class_name}) # 채용 공고 목록의 클래스명 (유동적)
+
     for job_card in job_cards:
+        div = job_card.find(recursive=False).find_all(recursive=False)[-1]
         job = {
             'title': job_card.find('h2', {'class': 'position_card_info_title'}).text.strip(),
-            'company': job_card.find('div', {'class': 'enZSuu'}).text.strip(),
-            'location': job_card.find('ul', {'class': 'DZqQv'}).text.strip(),
+            'company':  div.find(recursive=False).text.strip(),
+            'location': div.find_all('ul', recursive=False)[-1].find(recursive=False).text.strip(),
             'url': "https://" + urllib.parse.urlsplit(target_url).hostname + job_card.find('a')['href']
         }
         jobs.append(job)
@@ -58,7 +62,9 @@ with open('../config/config.json') as f:
     config = json.load(f)
 driver = create_browser_driver(config['driver']['location'])
 jobs = crawl_with_scroll_search_page(driver, config['crawler']['url'])
+for item in jobs:
+    print(item)
 
-for job in jobs:
-    description = crawl_hiring_post_detail_page(driver, job['url'])
-    print(description)
+# for job in jobs:
+#     description = crawl_hiring_post_detail_page(driver, job['url'])
+#     print(description)
