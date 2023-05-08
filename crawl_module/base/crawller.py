7a7b28609bbaa2eb9f6f97842f223a268411b493
JunLyul Yang <sjn3649@gmail.com>
2023-05-09 01:06:19 +0900

    크롤링 방식 수정 - 대체 왜 난 HTML을 파싱하려고 했던가... 마음이 급했나...

--- crawl_module/base/crawller.py
+++ crawl_module/base/crawller.py
@@ -1,70 +1,36 @@
 import json
-import urllib.parse
+import shutil
+import uuid
 
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
+import requests as requests
 
 
-# Chrome 기반 드라이버 반환
-def create_browser_driver(driver_location):
-    service = Service(driver_location)
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    return webdriver.Chrome(service=service, options=options)
+# 채용 공고 검색 API 파싱
+def parse_post_search_api():
+    url = config['api_url']['search']
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # 요청에 성공한 경우, response.json() 메서드를 사용하여 응답 데이터를 파싱합니다.
+        data = response.json()
+        return data.get('result').get('positions')
+
+    else:
+        print(f'Request failed with status code Status:{response.status_code}')
 
 
-def crawl_with_scroll_search_page(driver, target_url: str):
-    # 모든 채용 공고 로딩을 위한 스크롤
-    # FIXME : 스크롤 로딩 작동 안 함
-    driver.get(target_url)
-    last_height = driver.execute_script('return document.body.scrollHeight')
-    while True:
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        WebDriverWait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-        new_height = driver.execute_script('return document.body.scrollHeight')
-        if new_height == last_height:
-            break
-        last_height = new_height
+# 채용 공고 이미지 다운로드
+def download_image(url):
+    filename = "image_" + str(uuid.uuid4()) + ".jpg"
 
-    # 페이지 소스코드 추출
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    # 채용 별 div 요소 클래스 명 추출
-    jobs = []
-    job_section = soup.select('main section')[2]
-    card_class_name = job_section.find(recursive=False).get('class')[-1]
-    job_cards = soup.find_all('div', {'class': card_class_name}) # 채용 공고 목록의 클래스명 (유동적)
-
-    for job_card in job_cards:
-        div = job_card.find(recursive=False).find_all(recursive=False)[-1]
-        job = {
-            'title': job_card.find('h2', {'class': 'position_card_info_title'}).text.strip(),
-            'company':  div.find(recursive=False).text.strip(),
-            'location': div.find_all('ul', recursive=False)[-1].find(recursive=False).text.strip(),
-            'url': "https://" + urllib.parse.urlsplit(target_url).hostname + job_card.find('a')['href']
-        }
-        jobs.append(job)
-
-    return jobs
-
-
-def crawl_hiring_post_detail_page(driver, target_url: str):
-    driver.get(target_url)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    context = soup.select('position_info dl')
-    print(context)
-    return context[1:]
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(filename, "wb") as f:
+            response.raw.decode_content = True
+            shutil.copyfileobj(response.raw, f)
 
 
 with open('../config/config.json') as f:
     config = json.load(f)
-driver = create_browser_driver(config['driver']['location'])
-jobs = crawl_with_scroll_search_page(driver, config['crawler']['url'])
-for item in jobs:
-    print(item)
-
-# for job in jobs:
-#     description = crawl_hiring_post_detail_page(driver, job['url'])
-#     print(description)
+positions = parse_post_search_api()
+download_image(positions[0].get('imagePath'))
